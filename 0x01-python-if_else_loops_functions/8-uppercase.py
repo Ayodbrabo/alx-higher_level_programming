a1b9185aaccba521d7b9d2e5e8751bb701cfc95a
Ayo Okonye <ayookonye@gmail.com>
2023-11-28 12:16:22 -0800

    added~

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def uppercase(s):
-    for char in s:
-        if ord('a') <= ord(char) <= ord('z'):
-            print("{}".format(chr(ord(char) - ord('a') + ord('A'))), end='')
-        else:
-            print("{}".format(char), end='')
-    print()
+    """Print a string in uppercase."""
+    for c in s:
+        if ord(c) >= 97 and ord(c) <= 123:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
+#!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit)
-    return last_digit
-
-print_last_digit(123)
-print_last_digit(-456)
+    """Print the last digit of a number and return it"""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
